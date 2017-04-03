d70a6d02cd9617154f0f650bf144c656fae80bb0
kp <kristen.kod@gmail.com>
2017-04-03 17:30:41 +0200

    Edit file formulaire.php

---
+++ formulaire.php
@@ -0,0 +1,23 @@
+<!DOCTYPE>
+
+<!-- Créer un formulaire qui appellera la page protégée et lui enverra le mot de passe -->
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Page protégée par mdp</title>
+</head>
+<body>
+	<p>
+		Veuillez entrer votre mot de passe:<br />
+	</p>
+	
+	<form action="secret.php" method="post">
+	<p>
+		<input type="password" name="password" value="Mot de passe" />
+		<input type="submit" value="Valider" />
+	</p>
+	</form>
+	
+</body>
+</html>
